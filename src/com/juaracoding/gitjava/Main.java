be1a1a64028bc745d34ca741a163c511c1e20ee6
Elva Yundra Rindyana <elvayundra8@gmail.com>
2022-10-03 20:50:02 +0700

    Update feature

--- src/com/juaracoding/gitjava/Main.java
+++ src/com/juaracoding/gitjava/Main.java
@@ -2,6 +2,8 @@ package com.juaracoding.gitjava;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        System.out.println("Yuhuu");
     }
 }
